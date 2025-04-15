e5b797c8b62f2f94212f3daaf3361261bb51447d
Daniel-GC-23 <aloga.daniel.correa@gmail.com>
2025-04-15 15:42:34 -0600

    cambios

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,11 +1,13 @@
 import "./Cards.css";
 
-const Cards = ({ image, text}) => {
+const Cards = ({ image, text }) => {
     return (
         <div className="cards">
             <img className="cards__image" src={image} />
             <p className="cards__title">{text}</p>
-            <button className="cards__button"><box-icon className="transfer-icon" name="chevron-down" ></box-icon></button>
+            <div className="cards__button-container">
+                <button className="cards__button"><box-icon className="cards-icon" name="chevron-down" ></box-icon></button>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,30 +3,38 @@ import "./Home.css";
 import MainImage from "../../Components/MainImage/MainImage";
 import Cards from "../../Components/Cards/Cards";
 import imageHome from "../../Assets/imagenes/Home-cat.jpg";
+import homeCat from "../../Assets/imagenes/GatoPorcentaje.png";
 const Home = () => {
     return (
         <div className="home">
             <div>
                 <MainImage ruta={imageHome} textoPrincipal={"Miauland"} subtitulo={"Bienvenido a Miauland"} />
             </div>
-            <h1>Tu apoyo cuenta</h1>
-            <h3>Gracias a tu apoyo durante toda nuestra existencia hemos ayudado aproximadamente a:</h3>
-            <div>
-                <h1>+ 40 Gatos Rescatados</h1>
-                <img src="" alt="" />
-            </div>
-            <h1>Ronroneos de amor</h1>
-            <div>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
-            </div>
-            <h1>¿Por qué es mejor adoptar?</h1>
             <div className="home__container">
-                <Cards image={imageHome} text={"El Hallasgo del tesoro"} />
+                <h1 className="home__container-title">Tu apoyo cuenta</h1>
+                <h3 className="home__container-text">Gracias a tu apoyo durante toda nuestra existencia hemos ayudado aproximadamente a:</h3>
+                <div className="home__container-counter">
+                    <h1 className="home__container-counter-text">+ 40 Gatos Rescatados</h1>
+                    <img className="home__container-counter-img" src={homeCat} alt="" />
+                </div>
+                <h1 className="home__container-title-section">Ronroneos de amor</h1>
+                <div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                <h1 className="home__container_title-question">¿Por qué es mejor adoptar?</h1>
+                <div className="home__cards-container">
+                    <Cards image={imageHome} text={"El Hallasgo del tesoro"} />
+                    <Cards image={imageHome} text={"Desbloqueaste un logro épico"} />
+                    <Cards image={imageHome} text={"Sorpresa mejor que comida gratis"} />
+                    <Cards image={imageHome} text={"El fan número 1 de tu vida"} />
+                    <Cards image={imageHome} text={"Maestro del descanso extremo"} />
+                    <Cards image={imageHome} text={"¡Felicidad 24/7!"} />
+                </div>
             </div>
         </div>
     );
