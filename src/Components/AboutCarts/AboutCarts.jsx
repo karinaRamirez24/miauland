071ba8176923abc1b12edf8ed6ec0ab4cf9fad73
DanielGC23 <danie@DANIEL-PC>
2025-04-17 16:53:24 -0600

    Mejoras en estilos y estructura de AboutCarts y AboutUs
    Se ha añadido un nuevo estilo CSS para la clase `.aboutCarts`, incluyendo propiedades flexibles y una clase `.reverse` para el diseño dinámico.
    
    En `AboutCarts.jsx`, se implementó lógica para aplicar la clase `reverse` según el orden.
    
    Se actualizaron los estilos en `AboutUs.css`, cambiando la nomenclatura de clases a minúsculas y mejorando la presentación.
    
    En `AboutUs.jsx`, se reestructuró el contenido HTML, organizando mejor los elementos y añadiendo contenedores para misión, visión y fundador, así como el componente `AboutCarts` para detallar el proceso de rescate de gatos.
    
    Se corrigieron errores tipográficos y se mejoró la semántica y el estilo del contenido.

--- src/Components/AboutCarts/AboutCarts.jsx
+++ src/Components/AboutCarts/AboutCarts.jsx
@@ -1,8 +1,10 @@
 import "./AboutCarts.css";
 
 const AboutCarts = ({ image, title, conten, order }) => {
+    const orderClass = order === 2 ? "reverse" : "";
+
     return (
-        <div className="aboutCarts">
+        <div className={`aboutCarts ${orderClass}`}>
             <img className="aboutCarts__image" src={image} alt="" />
             <div className="aboutCarts__textContainer">
                 <h3 className="aboutCarts__textContainer-title">
--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -6,54 +6,66 @@ import MainImage from "../../Components/MainImage/MainImage";
 
 const AboutUs = () => {
   return (
-    <div className="AboutUs">
+    <div className="aboutUs">
       <div>
-        <MainImage ruta={imageHome} textoPrincipal={"Nosotros"} subtitulo={"Receta secreta de la felicidad: una pizca de ternura, ronroneos, cuatro patitas suaves y un tierno maullo."}
+        <MainImage
+          ruta={imageHome}
+          textoPrincipal={"Nosotros"}
+          subtitulo={
+            "Receta secreta de la felicidad: una pizca de ternura, ronroneos, cuatro patitas suaves y un tierno maullo."
+          }
         />
       </div>
-      <h1>¿Quienes somos?</h1>
-      <h3>
-        Nos dedicamos a<samp> resdcatar y resguardar </samp>gatitos abandonados.
-        Nuestra meta es encontrarles hogares seguros y darles una nueva vida.
-      </h3>
-
-      <div>
-        <h1>Mision</h1>
-        <h3>
-          Lorem ipsm dolor sit amet, consectert adipisciang elit, sed do eiusmod
-          tempor incididunt
+      <div className="aboutUs-home__container">
+        <h1  className="aboutUs-home__container-tiltle">¿Quienes somos?</h1>
+        <h3 className="aboutUs-home__container-text">
+          Nos dedicamos a<span className="aboutUs-home__text-bold"> rescatar y resguardar </span>gatitos
+          abandonados. Nuestra meta es encontrarles hogares seguros y darles una
+          nueva vida.
         </h3>
-      </div>
-      <div>
-        <h1>Vision</h1>
-        <h3>
-          Lorem ipsm dolor sit amet, consectert adipisciang elit, sed do eiusmod
-          tempor incididunt
-        </h3>
-      </div>
+        <div className="aboutUs-home__mision-container">
+          <h1 className="aboutUs-home__mision-title">Mision</h1>
+          <h3 className="aboutUs-home__mision-text">
+            Lorem ipsm dolor sit amet, consectert adipisciang elit, sed do
+            eiusmod tempor incididunt
+          </h3>
+        </div>
+        <div className="aboutUs-home__vision-container">
+          <h1 className="aboutUs-home__vision-title">Vision</h1>
+          <h3 className="aboutUs-home__vision-text">
+            Lorem ipsm dolor sit amet, consectert adipisciang elit, sed do
+            eiusmod tempor incididunt
+          </h3>
+        </div>
 
-      <div>
-      <h1>Sandra N</h1>
-      <h2>Fundadora</h2>
-      <img src="" alt="" />
-      <h3>
-        it is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layaut, The ponit of
-        using Lorem ipsumn is that is has a more-or-les norm distribution of
-        letters.
-      </h3>
+        <div className="aboutUs-home__conteiner-founder">
+          <h1 className="aboutUs-home__conteiner-founder-name">Sandra N</h1>
+          <h2 className="aboutUs-home__conteiner-founder-subtitle">Fundadora</h2>
+          <img className="aboutUs-home__conteiner-founder-photo" src={imageHome} alt="" />
+          <h3 className="aboutUs-home__conteiner-founder-history">
+            it is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layaut, The ponit
+            of using Lorem ipsumn is that is has a more-or-les norm distribution
+            of letters.
+          </h3>
+        </div>
+        <div className= "aboutUs-home__conteiner-quest">
+          <h1 className="aboutUs-home__conteiner-quest-title">Maullido al rescate</h1>
+          <h3 className="aboutUs-home__conteiner-quest-text">
+            Rescatar a un gatito requiere ternura, compromiso y unos simples
+            pasos para transformar vida.
+          </h3>
+          <AboutCarts image={imageHome} title={"1. Observar y analizar"} conten={"¿Está en la calle, atrapado en un lugar peligroso, o simplemente perdido?"} order={1} />
+          <AboutCarts image={imageHome} title={"2.- Gánate su confianza"} conten={"No ir directo a atraprlo porque se espantará. Hable de maneta calmada y tranquila."} order={2} />
+          <AboutCarts image={imageHome} title={"3.- Use una transportyadora"} conten={"Una vez capturado el gato, colóque en una caja de trasporte para gatos."} order={1} />
+          <AboutCarts image={imageHome} title={"4.- Revise el estado de salud del gato"} conten={"Lleva al gato a un veterinario para una revisión completa."} order={2} />
+          <AboutCarts image={imageHome} title={"5.- Refugio o adopción"} conten={"Lo llevaremos al refigio de confianza donde lo recibimos con amor."} order={1} />
+        </div>
+        <h1 className="aboutUs-home__conteiner-conclusion">
+          <span className="aboutUs-home__conteiner-conclusion-bold">¡Mision Cumplida!</span> Ya sea en un refugio con una familia,
+          tiene una segunda oportunidad.
+        </h1>
       </div>
-          <div>
-            <h1>Maullido al rescate</h1>
-            <h3>Rescatar a un gatito requiere ternura, compromiso y unos simples pasos para transformar vida.</h3>
-            <AboutCarts image={imageHome} title={"1. Observar y analizar"}  conten={"¿Está en la calle, atrapado en un lugar peligroso, o simplemente perdido?"}  order = {1}/>
-            <AboutCarts image={imageHome} title={"2.- Gánate su confianza"}  conten={"No ir directo a atraprlo porque se espantará. Hable de maneta calmada y tranquila."}  order = {1}/>
-            <AboutCarts image={imageHome} title={"3.- Use una transportyadora"}  conten={"Una vez capturado el gato, colóque en una caja de trasporte para gatos."}  order = {1}/>
-            <AboutCarts image={imageHome} title={"4.- Revise el estado de salud del gato"}  conten={"Lleva al gato a un veterinario para una revisión completa."}  order = {1}/>
-            <AboutCarts image={imageHome} title={"5.- Refugio o adopción"}  conten={"Lo llevaremos al refigio de confianza donde lo recibimos con amor."}  order = {1}/>
-
-          </div>
-          <h1><span>¡Mision Cumplida!</span> Ya sea en un refugio con una familia, tiene una segunda oportunidad.</h1>
     </div>
   );
 };
